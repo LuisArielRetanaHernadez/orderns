44bc0b810559ec14762cf2e9151d9e0aeea7556b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-31 18:33:34 -0600

    agreando eslint con el estilo standar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
-      
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
